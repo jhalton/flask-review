3944b5df097b548355b39c4241eb6367461f77d4
Jeanette <haltonjeanette@gmail.com>
2024-10-08 17:34:25 -0700

    lists, tuples, sets

---
+++ python-refresher/lists_tuples_sets/code.py
@@ -0,0 +1,7 @@
+l = ["Morticia", "Gomez", "Uncle Fester"] #list
+t = ("Morticia", "Gomez", "Uncle Fester") #tuple -> cannot be modified once created
+s = {"Morticia", "Gomez", "Uncle Fester"} #set -> unordered
+
+print(l[0])
+l.append("Wednesday")
+print(l)
