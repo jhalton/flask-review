abb1275ac15dcd982fec6fab4a957814a2a7823c
Jeanette <haltonjeanette@gmail.com>
2024-10-10 16:07:28 -0700

    the in keyword

---
+++ python-refresher/the_in_keyword/code.py
@@ -0,0 +1,8 @@
+friends = ["Ian", "Marcus", "Jon"]
+print("Marcus" in friends)
+print("Dayven" in friends)
+
+
+movies_watched = {"Hocus Pocus", "Addams Family Values", "It"}
+user_movie = input("Enter something you've watched recently: ")
+print(user_movie in movies_watched)
