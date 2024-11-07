459da469f2b99014b26d214aae5edc1306fbdf00
Jeanette <haltonjeanette@gmail.com>
2024-11-06 17:00:17 -0800

    imports in python

---
+++ python-refresher/imports_in_python/code.py
@@ -0,0 +1,3 @@
+from my_module import divide
+
+print(divide(9, 3))
---
+++ python-refresher/imports_in_python/my_module.py
@@ -0,0 +1,6 @@
+def divide(dividend, divisor):
+    return dividend / divisor
+
+print("my_module.py: ", __name__)
+
+import libs.mylib
