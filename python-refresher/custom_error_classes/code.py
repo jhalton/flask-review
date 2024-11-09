0f5c1480c4e852c76406deb3b60d113bc4bc9c3f
Jeanette <haltonjeanette@gmail.com>
2024-11-09 10:54:54 -0800

    custom error classes

---
+++ python-refresher/custom_error_classes/code.py
@@ -0,0 +1,30 @@
+class TooManyPagesReadError(ValueError):
+    pass
+
+
+class Book:
+    def __init__(self, name: str, page_count: int):
+        self.name = name
+        self.page_count = page_count
+        self.pages_read = 0
+
+    def __repr__(self):
+        return (
+            f"<Book {self.name}, read {self.pages_read} pages out of {self.page_count}>"
+        )
+    
+    def read(self, pages:int):
+        if self.pages_read + pages > self.page_count:
+            raise TooManyPagesReadError(
+                f"You tried to read {self.pages_read + pages} pages, but this book only has {self.page_count} pages."
+            )
+        self.pages_read += pages
+        print(f"You have now read {self.pages_read} pages out of {self.page_count}.")
+
+
+a_little_life = Book("A Little Life", 812)
+
+try:
+    a_little_life.read(900)
+except TooManyPagesReadError as e:
+    print(e)
