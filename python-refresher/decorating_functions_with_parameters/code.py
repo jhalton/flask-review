ee6788b56555638e72164aa84f81b690bb2c9517
Jeanette <haltonjeanette@gmail.com>
2024-11-09 12:39:48 -0800

    decorating functions with parameters

---
+++ python-refresher/decorating_functions_with_parameters/code.py
@@ -0,0 +1,26 @@
+import functools
+
+user = {"username": "jose", "access_level": "admin"}
+
+
+def make_secure(func):
+    @functools.wraps(func)
+    def secure_function(*args, **kwargs):
+        if user["access_level"] == "admin":
+            return func(*args, **kwargs)
+        else:
+            return f"No admin permissions"
+        
+    return secure_function
+
+@make_secure  
+def get_password(panel):
+    if panel == "admin":
+        return "1234"
+    elif panel == "billing":
+        return "super_secure_password"
+        
+
+get_admin_password = make_secure(get_password)
+
+print(get_admin_password())
