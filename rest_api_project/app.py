5055d8a1a59fee898643f4fe4f0731fccfa1f134
Jeanette <haltonjeanette@gmail.com>
2024-11-12 16:07:30 -0800

    Our first API

--- rest_api_project/app.py
+++ rest_api_project/app.py
@@ -1,3 +1,28 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+stores = [
+    {
+        "name": "Jeanette's Flowers",
+        "items": [
+            {
+                "name": "Sunflower",
+                "price": 8.99
+            },
+            {
+                "name": "Rose",
+                "price": 12.99
+            },
+            {
+                "name": "Daisy",
+                "price": 5.99
+            }
+        ]
+    }
+]
+
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}
