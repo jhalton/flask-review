53df4b86f0007ff6ff1611b8d773adcafd179a57
Jeanette <haltonjeanette@gmail.com>
2024-12-12 16:50:13 -0800

    Change how we store data from lists to dictionaries and made adjustments to functions to account for it

---
+++ rest_api_project/db.py
@@ -0,0 +1,15 @@
+stores = {}
+items = {
+    1: {
+        "name": "Sunflower",
+        "price": 8.99
+    },
+    2: {
+        "name": "Rose",
+        "price": 12.99
+    },
+    3: {
+        "name": "Daisy",
+        "price": 5.99
+    }
+}
