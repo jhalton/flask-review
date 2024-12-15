80e14ed39e32cd07f74d16b29b788d3ef1fc0599
Jeanette <haltonjeanette@gmail.com>
2024-12-14 16:51:08 -0800

    Error handling for creation type functions

--- rest_api_project/app.py
+++ rest_api_project/app.py
@@ -18,6 +18,16 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json() 
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload"
+        )
+    
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
+            
     store_id = uuid.uuid4().hex #will create a long string of numbers and letters
     store = {**store_data, "id": store_id}
     stores[store_id] = store
@@ -27,6 +37,24 @@ def create_store():
 @app.post("/item")
 def create_item(name):
     item_data = request.get_json()
+    # Here not only do we need to valdiate data exists,
+    # But also what type of data. Price should be a float,
+    # for example.
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400, message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload",
+        )
+    for item in item.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+
     if item_data["store_id"] not in stores:
         abort(404, message="Store not found.")
     
