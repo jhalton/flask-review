0842d5a405b67a3abfc069509177046158e11cff
Jeanette <haltonjeanette@gmail.com>
2024-12-18 17:17:39 -0800

    Added update_item function

--- rest_api_project/app.py
+++ rest_api_project/app.py
@@ -65,10 +65,6 @@ def create_item(name):
     return item, 201
 
 
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items. values())}
-
 
 @app.get("/store/<string:store_id>")
 def get_store(store_id):
@@ -91,3 +87,22 @@ def delete_item(item_id):
         return {"message": "Item deleted."}
     except KeyError:
         abort(404, message="Item not found.")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, "Bad request. Ensure 'price' and 'name' are included in the JSON payload.")
+
+    try:
+        item = items[item_id]
+        item |= item_data
+        
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items. values())}
